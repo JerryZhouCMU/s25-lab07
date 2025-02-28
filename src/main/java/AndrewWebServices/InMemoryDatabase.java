231e974369714d243b3256ee6f293a82e1efb66d
JerryZhouSirui <jerryzho@andrew.cmu.edu>
2025-02-28 09:37:40 -0500

    Update tests

--- src/main/java/AndrewWebServices/InMemoryDatabase.java
+++ src/main/java/AndrewWebServices/InMemoryDatabase.java
@@ -1,5 +1,8 @@
 package AndrewWebServices;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  * InMemoryDatabase is a fake for the AndrewWS database which is used to improve test efficiency.
  * Remember, fakes are fully functional classes with simplified implementation.
@@ -7,6 +10,19 @@ package AndrewWebServices;
  * 
  * Hint: there are two methods you need to implement
  */
-public class InMemoryDatabase /* should there be something here? */ {
+public class InMemoryDatabase extends Database /* should there be something here? */ {
     // Implement your fake database here
+    private Map<String, Integer> userMap = new HashMap<>();
+
+    // Override getPassword to return the stored password without delay
+    @Override
+    public int getPassword(String accountName) {
+        // Return 0 (or -1) if the user is not in the map
+        return userMap.getOrDefault(accountName, 0);
+    }
+
+    // A helper method to add a user and password to this fake database
+    public void addUser(String accountName, int password) {
+        userMap.put(accountName, password);
+    }
 }
--- src/test/java/AndrewWebServices/AndrewWebServicesTest.java
+++ src/test/java/AndrewWebServices/AndrewWebServicesTest.java
@@ -1,31 +1,58 @@
 package AndrewWebServices;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class AndrewWebServicesTest {
-    Database database;
-    RecSys recommender;
-    PromoService promoService;
+    //Database database;
+    InMemoryDatabase fakeDatabase;
+    //RecSys recommender;
+    RecSys recSysStub; // We need a stub here
+    //PromoService promoService;
+    PromoService mockPromoService; // We need a mock here
     AndrewWebServices andrewWebService;
 
     @Before
     public void setUp() {
         // You need to use some mock objects here
-        database = new Database(); // We probably don't want to access our real database...
-        recommender = new RecSys();
-        promoService = new PromoService();
+        //database = new Database(); // We probably don't want to access our real database...
+        // Instead, we can use a fake database
+        fakeDatabase = new InMemoryDatabase();
+        fakeDatabase.addUser("Scotty", 17214);
 
-        andrewWebService = new AndrewWebServices(database, recommender, promoService);
+        //recommender = new RecSys();
+
+        // We need a stub for RecSys
+        recSysStub = mock(RecSys.class);
+        when(recSysStub.getRecommendation(anyString()))
+            .thenReturn("Animal House"); 
+
+        //promoService = new PromoService();
+
+        // We need a mock for PromoService
+        mockPromoService = mock(PromoService.class);
+
+        //andrewWebService = new AndrewWebServices(database, recommender, promoService);
+        andrewWebService = new AndrewWebServices(fakeDatabase, recSysStub, mockPromoService);
     }
 
     @Test
     public void testLogIn() {
         // This is taking way too long to test
         assertTrue(andrewWebService.logIn("Scotty", 17214));
+        assertFalse("Expected login to fail w/ incorrect password",
+                    andrewWebService.logIn("Scotty", 99999));
+        assertFalse("Expected login to fail for unknown user",
+                    andrewWebService.logIn("NonUser", 12345));
     }
 
     @Test
@@ -38,11 +65,22 @@ public class AndrewWebServicesTest {
     public void testSendEmail() {
         // How should we test sendEmail() when it doesn't have a return value?
         // Hint: is there something from Mockito that seems useful here?
+
+        // We'll call the method
+        String testEmail = "bob@example.com";
+        andrewWebService.sendPromoEmail(testEmail);
+
+        // Verify that mockPromoService.mailTo(...) was called exactly once
+        verify(mockPromoService).mailTo(testEmail);
     }
 
     @Test
     public void testNoSendEmail() {
         // How should we test that no email has been sent in certain situations (like right after logging in)?
         // Hint: is there something from Mockito that seems useful here?
+        // 1. Perform some action that should NOT trigger an email
+        andrewWebService.logIn("Scotty", 17214);
+        // 2. Verify that mailTo(...) was never called
+        verify(mockPromoService, never()).mailTo(anyString());
     }
 }
